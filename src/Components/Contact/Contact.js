94a531c29e0386c69e1262403e75c5af52fbc9d0
shuja990 <58367243+shuja990@users.noreply.github.com>
2020-07-16 13:21:57 +0500

    Update Contact.js

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import emailjs from 'emailjs-com';
 import './Contact.css';
 import StatusAlert, { StatusAlertService } from 'react-status-alert';
@@ -11,7 +11,8 @@ function sendEmail(e) {
     	emailjs.sendForm('default_service', 'template_ixtvNRzX', e.target, 'user_DLaK4TDVt4KzTGl1oxNQk')
     	.then((result) => {
     		if(result.text==="OK"){
-    			const alertId = StatusAlertService.showSuccess('Email Sent');
+				const alertId = StatusAlertService.showSuccess('Email Sent');
+				console.log(alertId);
      		}
       	}, (error) => {
         console.log(error.text);
@@ -23,25 +24,25 @@ function sendEmail(e) {
 	    <form className="contact-form back pa4 pt0 white-80 tc ba b--dark-gray bw2" onSubmit={sendEmail}>
 			<div>
 			<h1 className="f1 b b mb2 pa4">Contact Us</h1>
-      		<p className="f3 b pb0 pa1">Please fill in your Contact information and we will get back to you as soon as possible</p>
+      		<p className="f3 b pb0 pa1">Please fill in your infromation and we will get back to you as soon as possible</p>
 			<div className="flex justify-center">
-				<img  className="pr2" alt="shujaali1234@gmail.com" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBkPSJNMCAzdjE4aDI0di0xOGgtMjR6bTYuNjIzIDcuOTI5bC00LjYyMyA1LjcxMnYtOS40NThsNC42MjMgMy43NDZ6bS00LjE0MS01LjkyOWgxOS4wMzVsLTkuNTE3IDcuNzEzLTkuNTE4LTcuNzEzem01LjY5NCA3LjE4OGwzLjgyNCAzLjA5OSAzLjgzLTMuMTA0IDUuNjEyIDYuODE3aC0xOC43NzlsNS41MTMtNi44MTJ6bTkuMjA4LTEuMjY0bDQuNjE2LTMuNzQxdjkuMzQ4bC00LjYxNi01LjYwN3oiLz48L3N2Zz4="/>
+				<img  className="pr2" alt="mail" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBkPSJNMCAzdjE4aDI0di0xOGgtMjR6bTYuNjIzIDcuOTI5bC00LjYyMyA1LjcxMnYtOS40NThsNC42MjMgMy43NDZ6bS00LjE0MS01LjkyOWgxOS4wMzVsLTkuNTE3IDcuNzEzLTkuNTE4LTcuNzEzem01LjY5NCA3LjE4OGwzLjgyNCAzLjA5OSAzLjgzLTMuMTA0IDUuNjEyIDYuODE3aC0xOC43NzlsNS41MTMtNi44MTJ6bTkuMjA4LTEuMjY0bDQuNjE2LTMuNzQxdjkuMzQ4bC00LjYxNi01LjYwN3oiLz48L3N2Zz4="/>
 				<p className=" f6">shujaali1234@gmail.com</p>
 			</div>
 			</div>
 			<label className="f4 b db mb2 pa2">Name</label>
-			<input name="user_name" aria-label="Name"
+			<input name="user_name" 
 			className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-60" 
 			required
 			type="text" aria-describedby="name-desc"/>
 			<label className="f4 b db pa2 mb2">Email</label>
-			<input id="user_email" name="user_email" aria-label="Email"
+			<input id="user_email" name="user_email" 
 			className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-60" 
 			required 
 			type="email" 
 			aria-describedby="name-desc"/>
 			<label className="f4 b db  pa2 mb2">Message</label>
-	    	<input required type="text" id="comment" aria-label="Message" name="message" className="b pa2 pb4 input-reset ba bg-transparent hover-bg-black hover-white w-60" 
+	    	<input required type="text" id="comment" name="message" className="b pa2 pb4 input-reset ba bg-transparent hover-bg-black hover-white w-60" 
 	    	aria-describedby="comment-desc"/>
 			<br/><br/>
 			<StatusAlert/> 
