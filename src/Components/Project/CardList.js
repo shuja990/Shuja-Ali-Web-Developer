ed239969724b73f4cd5e9c61685d59e90e316b01
shuja990 <58367243+shuja990@users.noreply.github.com>
2020-07-16 12:47:27 +0500

    Update CardList.js

--- src/Components/Project/CardList.js
+++ src/Components/Project/CardList.js
@@ -9,31 +9,31 @@ const CardList = ({projects}) =>{
 		<h1 className="mt2 mb0 baskerville bg-black white pa2 fw1 f1 hover-bg-near-black">PORTFOLIO</h1>
 		<div className="flex flex-wrap justify-center">
 					<Card 
-					key={0}
+					key={projects[0].key}
 					name={projects[0].name} 
 					path ={one} 
 					info={projects[0].info}
 					link={projects[0].link}/>
 					<Card 
-					key={1}
+					key={projects[1].key}
 					name={projects[1].name} 
 					path ={two} 
 					info={projects[1].info}
 					link={projects[1].link}/>
 						<Card 
-					key={2}
+					key={projects[2].key}
 					name={projects[2].name} 
 					path ={three} 
 					info={projects[2].info}
 					link={projects[2].link}/>
 					<Card 
-					key={3}
+					key={projects[3].key}
 					name={projects[3].name} 
 					path ={projects[3].path} 
 					info={projects[3].info}
 					link={projects[3].link}/>
 					<Card 
-					key={4}
+					key={projects[4].key}
 					name={projects[4].name} 
 					path ={projects[4].path} 
 					info={projects[4].info}
