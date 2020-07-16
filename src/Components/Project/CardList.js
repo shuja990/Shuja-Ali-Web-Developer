991397448fa14fea5e24279edd30561b4ad0f3e7
shuja990 <58367243+shuja990@users.noreply.github.com>
2020-07-16 13:12:29 +0500

    Update CardList.js

--- src/Components/Project/CardList.js
+++ src/Components/Project/CardList.js
@@ -1,43 +1,21 @@
 import React from 'react';
 import Card from './Card';
-import one from './one.png'
-import two from './two.png'
-import three from './three.png'
 const CardList = ({projects}) =>{
 	return(
 		<div>
 		<h1 className="mt2 mb0 baskerville bg-black white pa2 fw1 f1 hover-bg-near-black">PORTFOLIO</h1>
 		<div className="flex flex-wrap justify-center">
+				{projects.map((item,idx)=>(
 					<Card 
-					key={projects[0].key}
-					name={projects[0].name} 
-					path ={one} 
-					info={projects[0].info}
-					link={projects[0].link}/>
-					<Card 
-					key={projects[1].key}
-					name={projects[1].name} 
-					path ={two} 
-					info={projects[1].info}
-					link={projects[1].link}/>
-						<Card 
-					key={projects[2].key}
-					name={projects[2].name} 
-					path ={three} 
-					info={projects[2].info}
-					link={projects[2].link}/>
-					<Card 
-					key={projects[3].key}
-					name={projects[3].name} 
-					path ={projects[3].path} 
-					info={projects[3].info}
-					link={projects[3].link}/>
-					<Card 
-					key={projects[4].key}
-					name={projects[4].name} 
-					path ={projects[4].path} 
-					info={projects[4].info}
-					link={projects[4].link}/>
+					key={idx}
+					name={item.name} 
+					path ={item.path} 
+					info={item.info}
+					link={item.link}/>
+					
+				))
+				}
+					
 
 		</div> 
 		</div>
